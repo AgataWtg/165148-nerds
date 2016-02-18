21691255335eea36df8d345c889875a27f2319b6
AgataWtg <awasiljewa@gmail.com>
2016-02-19 02:46:42 +0300

    Анимация и детали

--- js/popup.js
+++ js/popup.js
@@ -21,12 +21,15 @@ link.addEventListener("click", function(event) {
 close.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.remove("modal-content-show");
+  popup.classList.remove("modal-error");
 });
 
 form.addEventListener("submit", function(event) {
   if (!username.value || !email.value) {
     event.preventDefault();
-    consol.log("Нужно ввести имя и e-mail");
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
   } else {
     localStorage.setItem("username", username.value);
   }
@@ -36,6 +39,7 @@ window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
     if (popup.classList.contains("modal-content-show")) {
       popup.classList.remove("modal-content-show");
+      popup.classList.remove("modal-error");
     }
   }
 });
